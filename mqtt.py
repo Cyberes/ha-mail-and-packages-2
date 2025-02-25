2e7daa907b2bd352f22a6041b8e68d822ec51237
Cyberes <cyberes@evulid.cc>
2025-02-25 16:35:14 -0700

    update systemd services

--- mqtt.py
+++ mqtt.py
@@ -11,10 +11,10 @@ logging.basicConfig(level=logging.INFO)
 
 MQTT_BROKER_HOST = os.getenv('MQTT_BROKER_HOST', '')
 MQTT_BROKER_PORT = int(os.getenv('MQTT_BROKER_PORT', 1883))
-MQTT_CLIENT_ID = os.getenv('MQTT_CLIENT_ID', 'feeder')
+MQTT_CLIENT_ID = os.getenv('MQTT_CLIENT_ID', 'mail-and-packages-2')
 MQTT_USERNAME = os.getenv('MQTT_USERNAME', '')
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD', '')
-MQTT_TOPIC_PREFIX = os.getenv('MQTT_TOPIC_PREFIX', 'amazon-packages')
+MQTT_TOPIC_PREFIX = os.getenv('MQTT_TOPIC_PREFIX', 'mail-and-packages-2')
 
 client = mqtt.Client(client_id=MQTT_CLIENT_ID)
 if MQTT_USERNAME and MQTT_PASSWORD:
